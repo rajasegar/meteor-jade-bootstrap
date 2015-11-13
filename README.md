33cc2f44e5a27d942d2a2a806f88e55a5a13a80b
rajasegar-c <rajasegarc@freshdesk.com>
2015-11-13 11:03:44 +0530

    adding documentation in README, package.js and versions

---
+++ README.md
@@ -0,0 +1,18 @@
+Bootstrap Components written using JADE mixins for Meteor
+==================
+
+[Bootstrap](http://getbootstrap.com/) using [Jade](http://jade-lang.com) packaged for [Meteor](https://www.meteor.com)
+
+## Install
+
+```bash
+$ meteor add rajasegar:jade-bootstrap
+```
+
+
+## Usage
+
+Complete documentation for this Meteor package is not available yet. You can refer to the original project documentation for [JADE-Bootstrap](http://rajasegar.github.io/JADE-Bootstrap/) here
+
+
+[JADE-Bootstrap](http://rajasegar.github.io/JADE-Bootstrap/)
--- package.js
+++ package.js
@@ -2,9 +2,9 @@ Package.describe({
   name: 'rajasegar:jade-bootstrap',
   version: '0.0.1',
   // Brief, one-line summary of the package.
-  summary: '',
+  summary: 'Bootstrap components written using JADE mixins for Meteor',
   // URL to the Git repository containing the source code for this package.
-  git: '',
+  git: 'https://github.com/rajasegar/meteor-jade-bootstrap.git',
   // By default, Meteor will default to using README.md for documentation.
   // To avoid submitting documentation, set this field to null.
   documentation: 'README.md'
@@ -14,6 +14,24 @@ Package.onUse(function(api) {
   api.versionsFrom('1.2.0.2');
   api.use('ecmascript');
   api.addFiles('jade-bootstrap.js');
+  api.addAssets([
+    'components/accordion.jade',
+    'components/alerts.jade',
+    'components/bootswatch.jade',
+    'components/carousel.jade',
+    'components/dropdowns.jade',
+    'components/forms.jade',
+    'components/icons.jade',
+    'components/images.jade',
+    'components/labels.jade',
+    'components/list_groups.jade',
+    'components/modal.jade',
+    'components/tabs.jade'
+  ],'client');
+  api.addFiles([
+    'components/accordion.js',
+    'components/carousel.js'
+  ]);
 });
 
 Package.onTest(function(api) {
